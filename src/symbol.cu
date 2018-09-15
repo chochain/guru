82f7080ddad339512c4b2f2c527b8df24474a63f
chochain <chochain@yahoo.com>
2018-09-15 18:45:49 +1200

    ~/guru refactor alloc value keyvalue symbol from c; break alloc into vmalloc

---
+++ src/symbol.cu
@@ -0,0 +1,227 @@
+/*! @file
+  @brief
+  Guru Symbol class
+
+  <pre>
+  Copyright (C) 2015-2018 Kyushu Institute of Technology.
+  Copyright (C) 2015-2018 Shimane IT Open-Innovation Center.
+
+  This file is distributed under BSD 3-Clause License.
+
+  </pre>
+*/
+#include "vm_config.h"
+#include <assert.h>
+
+#include "guru.hu"
+#include "alloc.hu"
+#include "symbol.hu"
+
+#if !defined(MRBC_SYMBOL_SEARCH_LINER) && !defined(MRBC_SYMBOL_SEARCH_BTREE)
+#define MRBC_SYMBOL_SEARCH_BTREE
+#endif
+
+#ifndef MRBC_SYMBOL_TABLE_INDEX_TYPE
+#define MRBC_SYMBOL_TABLE_INDEX_TYPE	uint16_t
+#endif
+
+struct SYM_INDEX {
+    uint16_t hash;	//!< hash value, returned by calc_hash().
+#ifdef MRBC_SYMBOL_SEARCH_BTREE
+    MRBC_SYMBOL_TABLE_INDEX_TYPE left;
+    MRBC_SYMBOL_TABLE_INDEX_TYPE right;
+#endif
+    const char *cstr;	//!< point to the symbol string.
+};
+
+__GURU__ struct SYM_INDEX sym_index[MAX_SYMBOLS_COUNT];
+__GURU__ int sym_index_pos;	// point to the last(free) sym_index array.
+
+//================================================================
+/*! search index table
+ */
+__GURU__ int search_index(uint16_t hash, const char *str)
+{
+#ifdef MRBC_SYMBOL_SEARCH_LINER
+    int i;
+    for(i = 0; i < sym_index_pos; i++) {
+        if (sym_index[i].hash==hash && strcmp(str, sym_index[i].cstr)==0) {
+            return i;
+        }
+    }
+    return -1;
+#endif
+
+#ifdef MRBC_SYMBOL_SEARCH_BTREE
+    int i = 0;
+    do {
+        if (sym_index[i].hash==hash && guru_strcmp(str, sym_index[i].cstr)==0) {
+            return i;
+        }
+        if (hash < sym_index[i].hash) {
+            i = sym_index[i].left;
+        } else {
+            i = sym_index[i].right;
+        }
+    } while(i != 0);
+    return -1;
+#endif
+}
+
+//================================================================
+/*! add to index table
+ */
+__GURU__ int add_index(uint16_t hash, const char *str)
+{
+    // check overflow.
+    if (sym_index_pos >= MAX_SYMBOLS_COUNT) {
+//        printf("Overflow %s for '%s'\n", "MAX_SYMBOLS_COUNT", str);
+        assert(1==0);
+        return -1;
+    }
+    int sym_id = sym_index_pos++;
+
+    // append table.
+    sym_index[sym_id].hash = hash;
+    sym_index[sym_id].cstr = str;
+
+#ifdef MRBC_SYMBOL_SEARCH_BTREE
+    int i = 0;
+
+    while(1) {
+        if (hash < sym_index[i].hash) {
+            // left side
+            if (sym_index[i].left==0) {	// left is empty?
+                sym_index[i].left = sym_id;
+                break;
+            }
+            i = sym_index[i].left;
+        } else {
+            // right side
+            if (sym_index[i].right==0) {	// right is empty?
+                sym_index[i].right = sym_id;
+                break;
+            }
+            i = sym_index[i].right;
+        }
+    }
+#endif
+    return sym_id;
+}
+
+//================================================================
+/*! constructor
+
+  @param  vm	pointer to VM.
+  @param  str	String
+  @return 	symbol object
+*/
+__GURU__ mrbc_value mrbc_symbol_new(struct VM *vm, const char *str)
+{
+    mrbc_value ret = {.tt = MRBC_TT_SYMBOL};
+    uint16_t h = calc_hash(str);
+    mrbc_sym sym_id = search_index(h, str);
+
+    if (sym_id >= 0) {
+        ret.i = sym_id;
+        return ret;		// already exist.
+    }
+
+    // create symbol object dynamically.
+    int size = guru_strlen(str) + 1;
+    char *buf = (char *)mrbc_raw_alloc(size);
+    if (buf==NULL) return ret;		// ENOMEM raise?
+
+    MEMCPY(buf, str, size);
+    ret.i = add_index(h, buf);
+
+    return ret;
+}
+
+//================================================================
+/*! Calculate hash value.
+
+  @param  str		Target string.
+  @return uint16_t	Hash value.
+*/
+__GURU__ uint16_t calc_hash(const char *str)
+{
+    uint16_t h = 0;
+
+    while(*str != '\0') {
+        h = h * 37 + *str;
+        str++;
+    }
+    return h;
+}
+
+//================================================================
+/*! Convert string to symbol value.
+
+  @param  str		Target string.
+  @return mrbc_sym	Symbol value.
+*/
+__GURU__ mrbc_sym str_to_symid(const char *str)
+{
+    uint16_t h = calc_hash(str);
+    mrbc_sym sym_id = search_index(h, str);
+    if (sym_id >= 0) return sym_id;
+
+    return add_index(h, str);
+}
+
+
+//================================================================
+/*! Convert symbol value to string.
+
+  @param  mrbc_sym	Symbol value.
+  @return const char*	String.
+  @retval NULL		Invalid sym_id was given.
+*/
+__GURU__ const char * symid_to_str(mrbc_sym sym_id)
+{
+    if (sym_id < 0) return NULL;
+    if (sym_id >= sym_index_pos) return NULL;
+
+    return sym_index[sym_id].cstr;
+}
+
+#if MRBC_USE_ARRAY
+//================================================================
+/*! (method) all_symbols
+ */
+__GURU__ void c_all_symbols(mrbc_value v[], int argc)
+{
+    mrbc_value ret = mrbc_array_new(sym_index_pos);
+
+    int i;
+    for(i = 0; i < sym_index_pos; i++) {
+        mrbc_value sym1 = {.tt = MRBC_TT_SYMBOL};
+        sym1.i = i;
+        mrbc_array_push(&ret, &sym1);
+    }
+    SET_RETURN(ret);
+}
+#endif
+
+#if MRBC_USE_STRING
+//================================================================
+/*! (method) inspect
+ */
+static void c_inspect(struct VM *vm, mrbc_value v[], int argc)
+{
+    const char *s = symid_to_str(v[0].i);
+    v[0] = mrbc_string_new_cstr(vm, ":");
+    mrbc_string_append_cstr(&v[0], s);
+}
+
+
+//================================================================
+/*! (method) to_s
+ */
+static void c_to_s(struct VM *vm, mrbc_value v[], int argc)
+{
+    v[0] = mrbc_string_new_cstr(vm, symid_to_str(v[0].i));
+}
+#endif
+
---
+++ src/vmalloc.cu
@@ -0,0 +1,233 @@
+/*! @file
+  @brief
+  mrubyc memory management.
+
+  <pre>
+  Copyright (C) 2015-2018 Kyushu Institute of Technology.
+  Copyright (C) 2015-2018 Shimane IT Open-Innovation Center.
+
+  This file is distributed under BSD 3-Clause License.
+
+  Memory management for objects in Guru.
+
+  </pre>
+*/
+#include "vm_config.h"
+#include <assert.h>
+#include "value.hu"
+#include "alloc.hu"
+#include "keyvalue.hu"
+#include "symbol.hu"
+#include "vmalloc.hu"
+
+// memory pool
+extern __GURU__ uint8_t *memory_pool;
+
+// << from value.cu
+__GURU__
+mrbc_object *mrbc_obj_alloc(mrbc_vtype tt)
+{
+    mrbc_object *ptr = (mrbc_object *)mrbc_alloc(sizeof(mrbc_object));
+    if (ptr){
+        ptr->tt = tt;
+    }
+    return ptr;
+}
+
+__GURU__
+mrbc_proc *mrbc_rproc_alloc(const char *name)
+{
+    mrbc_proc *ptr = (mrbc_proc *)mrbc_alloc(sizeof(mrbc_proc));
+    if (ptr) {
+        ptr->ref_count = 1;
+        ptr->sym_id = str_to_symid(name);
+#ifdef MRBC_DEBUG
+        ptr->names = name;	// for debug; delete soon.
+#endif
+        ptr->next = 0;
+    }
+    return ptr;
+}
+
+//================================================================
+/*!@brief
+  Release object related memory
+
+  @param   v     Pointer to target mrbc_value
+*/
+__GURU__
+void mrbc_release(mrbc_value *v)
+{
+    mrbc_dec_ref_counter(v);
+    v->tt = MRBC_TT_EMPTY;
+}
+
+//================================================================
+/*! mrbc_instance constructor
+
+  @param  vm    Pointer to VM.
+  @param  cls	Pointer to Class (mrbc_class).
+  @param  size	size of additional data.
+  @return       mrbc_instance object.
+*/
+__GURU__
+mrbc_value mrbc_instance_new(mrbc_class *cls, int size)
+{
+    mrbc_value v = {.tt = MRBC_TT_OBJECT};
+    v.instance = (mrbc_instance *)mrbc_alloc(sizeof(mrbc_instance) + size);
+    if (v.instance == NULL) return v;	// ENOMEM
+
+    v.instance->ivar = mrbc_kv_new(0);
+    if (v.instance->ivar == NULL) {	// ENOMEM
+        mrbc_raw_free(v.instance);
+        v.instance = NULL;
+        return v;
+    }
+
+    v.instance->ref_count = 1;
+    v.instance->tt = MRBC_TT_OBJECT;	// for debug only.
+    v.instance->cls = cls;
+
+    return v;
+}
+
+//================================================================
+/*! mrbc_instance destructor
+
+  @param  v	pointer to target value
+*/
+__GURU__
+void mrbc_instance_delete(mrbc_value *v)
+{
+    mrbc_kv_delete(v->instance->ivar);
+    mrbc_raw_free(v->instance);
+}
+
+
+//================================================================
+/*! instance variable setter
+
+  @param  obj		pointer to target.
+  @param  sym_id	key symbol ID.
+  @param  v		pointer to value.
+*/
+__GURU__
+void mrbc_instance_setiv(mrbc_object *obj, mrbc_sym sym_id, mrbc_value *v)
+{
+    mrbc_dup(v);
+    mrbc_kv_set(obj->instance->ivar, sym_id, v);
+}
+
+
+//================================================================
+/*! instance variable getter
+
+  @param  obj		pointer to target.
+  @param  sym_id	key symbol ID.
+  @return		value.
+*/
+__GURU__
+mrbc_value mrbc_instance_getiv(mrbc_object *obj, mrbc_sym sym_id)
+{
+    mrbc_value *v = mrbc_kv_get(obj->instance->ivar, sym_id);
+    if (!v) return mrbc_nil_value();
+
+    mrbc_dup(v);
+    return *v;
+}
+
+//================================================================
+/*!@brief
+  Decrement reference counter
+
+  @param   v     Pointer to target mrbc_value
+*/
+__GURU__
+void mrbc_dec_ref_counter(mrbc_value *v)
+{
+    switch(v->tt){
+    case MRBC_TT_OBJECT:
+    case MRBC_TT_PROC:
+    case MRBC_TT_ARRAY:
+    case MRBC_TT_STRING:
+    case MRBC_TT_RANGE:
+    case MRBC_TT_HASH:
+        assert(v->instance->ref_count != 0);
+        v->instance->ref_count--;
+        break;
+
+    default:
+        // Nothing
+        return;
+    }
+
+    // release memory?
+    if (v->instance->ref_count != 0) return;
+
+    switch(v->tt) {
+    case MRBC_TT_OBJECT:	mrbc_instance_delete(v);	break;
+    case MRBC_TT_PROC:	mrbc_raw_free(v->handle);	break;
+#if MRBC_USE_STRING
+    case MRBC_TT_STRING:	mrbc_string_delete(v);		break;
+#endif
+#if MRBC_USE_ARRAY
+    case MRBC_TT_ARRAY:	mrbc_array_delete(v);		break;
+    case MRBC_TT_RANGE:	mrbc_range_delete(v);		break;
+    case MRBC_TT_HASH:	mrbc_hash_delete(v);		break;
+#endif
+    default:
+        // Nothing
+        break;
+    }
+}
+
+//================================================================
+/*! set vm id
+
+  @param  ptr	Return value of mrbc_alloc()
+  @param  vm_id	vm id
+*/
+__GURU__
+void mrbc_set_vm_id(void *ptr, int vm_id)
+{
+    SET_VM_ID(ptr, vm_id);
+}
+
+//================================================================
+/*! get vm id
+
+  @param  ptr	Return value of mrbc_alloc()
+  @return int	vm id
+*/
+__GURU__
+int mrbc_get_vm_id(void *ptr)
+{
+    return GET_VM_ID(ptr);
+}
+
+//================================================================
+/*!@brief
+  clear vm id
+
+  @param   v     Pointer to target mrbc_value
+*/
+__GURU__
+void mrbc_clear_vm_id(mrbc_value *v)
+{
+    switch(v->tt) {
+#if MRBC_USE_STRING
+    case MRBC_TT_STRING: mrbc_string_clear_vm_id(v);	break;
+#endif
+#if MRBC_USE_ARRAY
+    case MRBC_TT_ARRAY:	mrbc_array_clear_vm_id(v);	break;
+    case MRBC_TT_RANGE:	mrbc_range_clear_vm_id(v);	break;
+    case MRBC_TT_HASH:	mrbc_hash_clear_vm_id(v);	break;
+#endif
+    default:
+        // Nothing
+        break;
+    }
+}
+
+
+
