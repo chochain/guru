5d59d78b057038e592f43d7e33b99197ed797625
chochain <chochain@yahoo.com>
2018-12-17 20:43:33 +1300

    ~/guru refactor out gurux, vmx for interfacing

---
+++ src/gurux.h
@@ -0,0 +1,43 @@
+/*! @file
+  @brief
+  Guru value definitions
+
+  <pre>
+  Copyright (C) 2015-2018 Kyushu Institute of Technology.
+  Copyright (C) 2015-2018 Shimane IT Open-Innovation Center.
+
+  This file is distributed under BSD 3-Clause License.
+  </pre>
+*/
+#ifndef GURU_SRC_GURUX_H_
+#define GURU_SRC_GURUX_H_
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct guru_ses_ {
+	uint8_t *in;
+	uint8_t *out;
+	uint8_t id;
+	uint8_t trace;
+	struct guru_ses_ *next;
+} guru_ses;
+
+// mrbc types
+typedef int32_t 	mrbc_int;
+typedef float 		mrbc_float;
+typedef int16_t 	mrbc_sym;
+
+int guru_system_setup(int trace);
+int guru_system_run(int trace);
+
+int session_add(guru_ses *ses, const char *rite_fname, int trace);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ src/vmx.h
@@ -0,0 +1,34 @@
+/*! @file
+  @brief
+  mruby bytecode executor.
+
+  <pre>
+  Copyright (C) 2015-2017 Kyushu Institute of Technology.
+  Copyright (C) 2015-2017 Shimane IT Open-Innovation Center.
+
+  This file is distributed under BSD 3-Clause License.
+
+  Fetch mruby VM bytecodes, decode and execute.
+
+  </pre>
+*/
+
+#ifndef GURU_SRC_VMX_H_
+#define GURU_SRC_VMX_H_
+
+#include "gurux.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+cudaError_t guru_vm_setup(guru_ses *ses, int trace);
+cudaError_t guru_vm_run(guru_ses *ses, int trace);
+cudaError_t guru_vm_release(guru_ses *ses, int trace);
+cudaError_t guru_vm_trace(int level);
+
+#ifdef __cplusplus
+}
+#endif
+#endif
+
