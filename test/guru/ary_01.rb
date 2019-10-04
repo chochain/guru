3cc2fdb0d6df5ee673a53d73a43f8bbfcfe9e754
chochain <chochain@yahoo.com>
2019-10-04 13:31:33 +0800

    ~/guru/test update rng, ary iterators

--- test/guru/ary_01.rb
+++ test/guru/ary_01.rb
@@ -1,14 +1,23 @@
 # Array test for following methods:
 a = []
-p    a.to_s
 puts a.to_s
+puts a
+p a
 
 a = [1,2,3]
-
-p    a    
-puts a
 puts a.to_s
+puts a
 puts a.inspect
+p a
     
-b = ["1", "2","3"]
+b = ["1","2","3"]
 puts b.to_s
+puts b
+puts b.inspect
+p b
+
+c = [1,2,3,["4",5]]
+puts c
+p c
+
+    
--- test/guru/base_05.rb
+++ test/guru/base_05.rb
@@ -4,5 +4,9 @@
 #   SETUPVAR, GETUPVAR
 
 for i in 1..5 do
-  puts i
+    puts i
+end
+
+[ 1, 2, "X" ].each do |i|
+    puts i
 end
