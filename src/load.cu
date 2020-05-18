670f9137434d9dcf318b415b7e51273dbac27007
gnii <chochain@yahoo.com>
2020-05-17 20:04:17 -0700

    load: parse_bytecode returns U8* instead of GRIT*

--- src/load.cu
+++ src/load.cu
@@ -305,7 +305,7 @@ _fill_grit(GRIT *gr, rite_size *sz, int ix, U8 **bp)
 	}
 }
 
-__CODE__ GRIT*
+__CODE__ U8*
 _build_image(U8 **bp)
 {
     U8   *p  = *bp;
@@ -314,7 +314,7 @@ _build_image(U8 **bp)
     rite_size sz { .rsz=1, .psz=0, .isz=0, .ssz=0 };
     _fill_grit(gr, &sz, 0, bp);
 
-	return gr;
+	return (U8*)gr;
 }
 
 //================================================================
@@ -344,13 +344,13 @@ _load_lvar(U8 **bp)
   @param  src	Pointer to bytecode.
 
 */
-__CODE__ GRIT*
+__CODE__ U8*
 parse_bytecode(U8 *src)
 {
 	U8  **bp = (U8 **)&src;			// a pointer to pointer, so that we can pass and adjust the pointer
 	int ret  = _check_version(bp);
 
-	GRIT *gr;
+	U8 *gr;
     while (ret==NO_ERROR) {
         if (__MEMCMP(*bp, "IREP", 4)==0) {
         	*bp += 4 + sizeof(U32);								// skip "IREP", irep_sz
