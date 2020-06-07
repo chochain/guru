56a996889030287de9a4be41919d454c490ee459
gnii <chochain@yahoo.com>
2020-06-07 11:25:19 -0700

    iter: refactor it (iterator) to ix for pointers

--- src/iter.cu
+++ src/iter.cu
@@ -29,35 +29,35 @@
 __GURU__ GR
 guru_iter_new(GR *obj, GR *step)
 {
-    guru_iter *it = (guru_iter *)guru_alloc(sizeof(guru_iter));
-    it->rc   = 1;
-    it->n    = obj->gt;			// reuse the field
-    it->step = step;
-    it->range= ref_inc(obj);
+    guru_iter *ix = (guru_iter *)guru_alloc(sizeof(guru_iter));
+    ix->rc   = 1;
+    ix->n    = obj->gt;			// reuse the field
+    ix->step = step;
+    ix->range= ref_inc(obj);
 
-    GR r { GT_ITER, ACL_HAS_REF, 0, MEMOFF(it) };
+    GR r { GT_ITER, ACL_HAS_REF, 0, MEMOFF(ix) };
     switch (obj->gt) {
     case GT_INT: {
-    	it->i	= obj->i;
-    	it->inc = (obj->i=0, obj);
+    	ix->i	= obj->i;
+    	ix->inc = (obj->i=0, obj);
     } break;
     case GT_RANGE: {
     	guru_range *r = GR_RNG(obj);
     	ASSERT(r->first.gt==GT_INT || r->first.gt==GT_FLOAT);
 
-    	it->i    = 0;
-    	it->inc  = guru_gr_alloc(1);
-    	*it->inc = r->first;
+    	ix->i    = 0;
+    	ix->inc  = guru_gr_alloc(1);
+    	*ix->inc = r->first;
     } break;
     case GT_ARRAY: {
     	guru_array *a = GR_ARY(obj);
-    	it->i    = 0;
-    	it->inc  = ref_inc(a->data);
+    	ix->i    = 0;
+    	ix->inc  = ref_inc(a->data);
     } break;
     case GT_HASH: {
     	guru_hash *h = GR_HSH(obj);
-    	it->i	 = 0;
-    	it->inc  = ref_inc(h->data);	ref_inc(h->data+1);
+    	ix->i	 = 0;
+    	ix->inc  = ref_inc(h->data);	ref_inc(h->data+1);
     } break;
     default: ASSERT(1==0);			// TODO: other types not supported yet
     }
@@ -71,45 +71,45 @@ guru_iter_next(GR *r)
 {
 	ASSERT(r->gt==GT_ITER);
 
-	guru_iter *it = GR_ITR(r);
+	guru_iter *ix = GR_ITR(r);
 	U32 nvar;
-	switch (it->n) {				// ranging object type (field reused)
+	switch (ix->n) {				// ranging object type (field reused)
 	case GT_INT: {
-		it->inc->i += it->step ? it->step->i : 1;
-		nvar = (it->inc->i < it->i);
+		ix->inc->i += ix->step ? ix->step->i : 1;
+		nvar = (ix->inc->i < ix->i);
 	} break;
 	case GT_RANGE: {
-		guru_range *r = GR_RNG(it->range);
+		guru_range *r = GR_RNG(ix->range);
 		U32 keep;
-		if (it->inc->gt==GT_FLOAT) {
-			it->inc->f += (it->step ? it->step->f : 1.0f);
-			keep = IS_INCLUDE(r) ? (it->inc->f <= r->last.f) : (it->inc->f < r->last.f);
+		if (ix->inc->gt==GT_FLOAT) {
+			ix->inc->f += (ix->step ? ix->step->f : 1.0f);
+			keep = IS_INCLUDE(r) ? (ix->inc->f <= r->last.f) : (ix->inc->f < r->last.f);
 		}
 		else {
-			it->inc->i += (it->step ? it->step->i : 1);
-			keep = IS_INCLUDE(r) ? (it->inc->i <= r->last.i) : (it->inc->i < r->last.i);
+			ix->inc->i += (ix->step ? ix->step->i : 1);
+			keep = IS_INCLUDE(r) ? (ix->inc->i <= r->last.i) : (ix->inc->i < r->last.i);
 		}
 		nvar = (keep) ? 1 : 0;
 	} break;
 	case GT_ARRAY: {
-		guru_array *a = GR_ARY(it->range);
-		GR         *d = &a->data[it->i];
+		guru_array *a = GR_ARY(ix->range);
+		GR         *d = &a->data[ix->i];
 		ref_dec(d);
-		if ((it->i + 1) < a->n) {
-			it->i += 1;
-			it->inc = ref_inc(++d);
+		if ((ix->i + 1) < a->n) {
+			ix->i += 1;
+			ix->inc = ref_inc(++d);
 			nvar  = 1;
 		}
 		else nvar=0;
 	} break;
 	case GT_HASH: {
-		guru_hash *h = GR_HSH(it->range);
-		GR        *d = &h->data[it->i];
+		guru_hash *h = GR_HSH(ix->range);
+		GR        *d = &h->data[ix->i];
 		ref_dec(d);
 		ref_dec(d+1);
-		if ((it->i+2) < h->n) {
-			it->i += nvar = 2;
-			it->inc = ref_inc(d+=2);	ref_inc(d+1);
+		if ((ix->i+2) < h->n) {
+			ix->i += nvar = 2;
+			ix->inc = ref_inc(d+=2);	ref_inc(d+1);
 		}
 		else nvar=0;
 	} break;
@@ -127,12 +127,12 @@ __GURU__ void
 guru_iter_del(GR *r)
 {
 	ASSERT(r->gt==GT_ITER);
-	guru_iter *it = GR_ITR(r);
+	guru_iter *ix = GR_ITR(r);
 
-	if (it->n==GT_RANGE) guru_free(it->inc);
+	if (ix->n==GT_RANGE) guru_free(ix->inc);
 
-    ref_dec(it->range);
-    guru_free(it);
+    ref_dec(ix->range);
+    guru_free(ix);
 
     *r = EMPTY;
 }
